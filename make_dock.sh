4df46834bef79ccaaab5397ccc37198a4731c076
chenyingcai <cheningcai@outlook.com>
2018-03-18 17:40:23 +0100

    clear and remain the origin file

---
+++ make_dock.sh
@@ -0,0 +1,2 @@
+#!bin/sh
+docker build -t githubblog/jupyter_alpine:simple -f dockerfile_githubblog_alpine_jupyter .
--- run_page_container.sh
+++ run_page_container.sh
@@ -3,7 +3,7 @@
 docker run -d \
 -p 8888:8888 -p 4000:4000 \
 -v $PWD:/opt/notebook \
--v ~/.ssh:/boot/.ssh githubblog/jupyter_alpine:v2
+-v ~/.ssh:/boot/.ssh githubblog/jupyter_alpine:simple
 # port 8888是用于jupyter界面的端口 port 4000 是用于预览生成的博客的端口
 # 挂载容器1 : 本地(注意: 此时本地地址应该处于博客项目的文件夹里) 对应容器里的/opt/notebook位置
 # 挂载ssh key, 注意自己的ssh key所在本地的位置, ubuntu一般位于~/.ssh
